069460bc00b957d0b2aba4e8f276e56c687823f5
서진 이 <heart080324@gmail.com>
2025-04-07 02:28:00 +0000

    반복 추가

--- myif.py
+++ myif.py
@@ -33,3 +33,25 @@ elif choice == "3":
     print(ascii_art_3)
 else:
     print("잘못 입력하셨습니다!")
+
+    while True:
+    print("\n숫자를 입력하세요 (1~3, 종료하려면 0 입력):")
+    choice = input("입력: ")
+
+    if choice == "0":
+        print("프로그램을 종료합니다!")
+        break
+    elif choice == "1":
+        print(ascii_art_1)
+    elif choice == "2":
+        print(ascii_art_2)
+    elif choice == "3":
+        print(ascii_art_3)
+    else:
+        print("잘못 입력하셨습니다!")
+
+# 동물 그림 출력 프로그램이 총 5번 반복 실행될 수 있게 만드시오.
+
+# 위 프로그램을 완성한 사람은 프로그램이 계속(무한) 반복하게 하고
+# 만약에 0을 입력하면 종료 되는 프로그램을 만드시오.
+

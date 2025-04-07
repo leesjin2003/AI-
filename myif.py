9b72b06e46af8edc8db6521eb5eb03ebe919757b
서진 이 <heart080324@gmail.com>
2025-04-07 02:33:00 +0000

    반복 추가

--- myif.py
+++ myif.py
@@ -6,6 +6,7 @@
 # 잘못 입력하면 잘못 입력했다고 출력
 
 # 아스키 아트 캐릭터 정의
+# 아스키 아트 캐릭터 정의
 ascii_art_1 = """
   (•_•)
   <)   )╯
@@ -21,20 +22,8 @@ ascii_art_3 = """
   ( •_•)>⌐■-■
 """
 
-# 사용자 입력 받기
-choice = input("숫자를 입력하세요 (1~3): ")
-
-# 입력값에 따라 출력
-if choice == "1":
-    print(ascii_art_1)
-elif choice == "2":
-    print(ascii_art_2)
-elif choice == "3":
-    print(ascii_art_3)
-else:
-    print("잘못 입력하셨습니다!")
-
-    while True:
+# 무한 반복
+while True:
     print("\n숫자를 입력하세요 (1~3, 종료하려면 0 입력):")
     choice = input("입력: ")
 
--- test3.py
+++ test3.py
@@ -32,48 +32,23 @@ def print_rabbit():
     """
     print(rabbit)
 
-# 프로그램 시작
-print("그림 출력 프로그램")
-print("=================")
-print("1. 고양이")
-print("2. 강아지")
-print("3. 토끼")
-print("=================")
-
-# 사용자 입력 받기
-n = int(input("선택: "))
-
-# 입력값에 따라 출력
-if n == 1:
-    print("고양이")
-    print_cat()
-elif n == 2:
-    print("강아지")
-    print_dog()
-elif n == 3:
-    print("토끼")
-    print_rabbit()
-else:
-    print("잘못 입력하셨습니다!")
-
-
-    for i in range(5):
-    print(f"\n[{i+1}/5] 숫자를 입력하세요 (1~3):")
-    choice = input("입력: ")
+for i in range(5):
+    print("\n동물 그림 출력 프로그램")
+    print("=================")
+    print("1. 고양이")
+    print("2. 강아지")
+    print("3. 토끼")
+    print("=================")
+    choice = input(f"[{i+1}/5] 선택 (1~3): ")
 
     if choice == "1":
-        print(ascii_art_1)
+        print("고양이")
+        print_cat()
     elif choice == "2":
-        print(ascii_art_2)
+        print("강아지")
+        print_dog()
     elif choice == "3":
-        print(ascii_art_3)
+        print("토끼")
+        print_rabbit()
     else:
         print("잘못 입력하셨습니다!")
-#0이 입력되면 프로그램 종료
-
-
-
-# 동물 그림 출력 프로그램이 총 5번 반복 실행될 수 있게 만드시오.
-
-# 위 프로그램을 완성한 사람은 프로그램이 계속(무한) 반복하게 하고
-# 만약에 0을 입력하면 종료 되는 프로그램을 만드시오.
